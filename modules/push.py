1ec046a3fd32710d3814724319e2cbfd9731b5de
Anton Glukhov <toxxinus@gmail.com>
2015-01-29 01:29:35 +0300

    Migrate push to support authentication

--- modules/push.py
+++ modules/push.py
@@ -1,8 +1,11 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
+
 __author__ = 'Anton Glukhov'
+__copyright__ = "Copyright 2014, Easywhere"
+__email__ = "ag@easywhere.ru"
 
 
-from flask.ext.login import login_required, login_user
+from flask.ext.login import login_required, current_user
 
 from TrackerRestApi import jsonrpc
 from TrackerRestApi import Session
@@ -12,18 +15,19 @@ from sqlautocode_gen.model import *
 
 
 @jsonrpc.method('addPushToken(user_id=Number,hardware_id=String,platform=String,token=String) -> Object', validate=True, authenticated=False)
+@login_required
 def addPushToken(user_id,hardware_id,platform,token):
 
     session = Session()
 
-    push = session.query(TrPushToken).filter(TrPushToken.user_id == user_id).filter(TrPushToken.hardware_id == hardware_id).first()
+    push = session.query(TrPushToken).filter(TrPushToken.user_id == int(current_user.get_id())).filter(TrPushToken.hardware_id == hardware_id).first()
 
     if push is None:
         """Add new push token"""
         p = TrPushToken(hardware_id=hardware_id,
                         platform=platform,
                         token=token,
-                        user_id=user_id)
+                        user_id=int(current_user.get_id()))
 
         try:
             session.add(p)
