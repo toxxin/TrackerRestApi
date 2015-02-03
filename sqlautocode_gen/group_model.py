3a1304598ae6eaa249dcff6bcc6bbf2bff141f82
Anton Glukhov <toxxinus@gmail.com>
2015-02-04 02:00:13 +0300

    Update group_model.py

--- sqlautocode_gen/group_model.py
+++ sqlautocode_gen/group_model.py
@@ -10,10 +10,10 @@ from sqlautocode_gen import DeclarativeBase
 
 
 association_table_user_group = Table('tr_user_group', DeclarativeBase.metadata,
-Column(u'id', Integer, primary_key=True),
-Column(u'user_id', Integer, ForeignKey('tr_user.id'), nullable=False),
-Column(u'group_id', Integer, ForeignKey('tr_group.id'), nullable=False)
-)
+    Column(u'id', Integer, primary_key=True),
+    Column(u'user_id', Integer, ForeignKey('tr_user.id'), nullable=False),
+    Column(u'group_id', Integer, ForeignKey('tr_group.id'), nullable=False)
+    )
 
 
 class TrGroup(DeclarativeBase):
@@ -24,18 +24,19 @@ class TrGroup(DeclarativeBase):
     #colomn definition
     id = Column(u'id', Integer, primary_key=True)
     title = Column(u'title', String(255), nullable=False)
-    desc = Column(u'desc', String(255), nullable=False)
+    desc = Column(u'desc', String(255))
     pic = Column(u'pic', String(255), nullable=False)
     invitation = Column(u'invitation', Boolean, nullable=False, default=False)
     meeting = Column(u'meeting', Boolean, nullable=False, default=False)
     help = Column(u'help', Boolean, nullable=False, default=False)
     last_modified = Column(u'last_modified', TIMESTAMP(), nullable=False, onupdate=func.now())
     creation_date = Column(u'creation_date', TIMESTAMP(), nullable=False, default=func.now())
+    user_id = Column(u'user_id', Integer, ForeignKey('tr_user.id'), nullable=False)
 
     #relation definitions
-    user_id = relationship("TrUser", foreign_keys="TrUser.id", cascade="all,delete")
+    users = relationship("TrUser", secondary=association_table_user_group)
 
-    def __init__(self, user_id, title, desc, pic=None, invitation=False, meeting=False, help=False):
+    def __init__(self, user_id, title, desc=None, pic="Default", invitation=False, meeting=False, help=False):
         self.title = title
         self.desc = desc
         self.pic = pic
