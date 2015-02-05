a60f8c3b19194c8b8b7635f69ba56052a2fb9fb4
Anton Glukhov <toxxinus@gmail.com>
2015-02-05 23:34:11 +0300

    Add group params

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,6 @@
 __author__ = 'Anton Glukhov'
+__copyright__ = "Copyright 2014, Easywhere"
+__email__ = "ag@easywhere.ru"
 
 device_params = ["time_interval", "accel_stat"]
 
@@ -9,6 +11,12 @@ geo_mandatory_params = ["name", "shape", "center", "radius"]
 geo_option_params = ["color", "state", ]
 geo_params_must_for_circus_or_square = ["center", "radius"]
 geo_params_must_for_rand = ["vertices"]
+
+place_mandatory_params = ["title", "longitude", "latitude", "type"]
+place_option_params = ["desc"]
+
+group_mandatory_params = ["title"]
+group_option_params = ["desc", "invitation", "meeting", "help"]
 
 GEO_SHAPE_CIRCUS = 0
 GEO_SHAPE_SQUARE = 1
