d3c9890a91dec6d7214975f857b40deb7114eaf6
Anton Glukhov <toxxinus@gmail.com>
2015-04-28 23:14:02 +0300

    Add unit test for getFeedNews

--- tests/feed.py
+++ tests/feed.py
@@ -345,6 +345,77 @@ class FeedUnsubTestCase(BaseTestCase):
         s.close()
 
 
+class FeedGetNewsTestCase(BaseTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        session = Session()
+
+        cls.u1 = TrUser(login="11111", type="phone", auth_code="1111", authenticated=True)
+        cls.u2 = TrUser(login="22222", type="phone", auth_code="2222", authenticated=True)
+        cls.u3 = TrUser(login="33333", type="phone", auth_code="3333", authenticated=True)
+        user_list = [cls.u1, cls.u2, cls.u3]
+        cls.user_count = len(user_list)
+        session.add_all(user_list)
+        session.flush()
+        session.refresh(cls.u1)
+        session.refresh(cls.u2)
+        session.commit()
+
+        cls.f1 = TrFeed("ttl1", "l1.com", "A", "http://pic1.com")
+        cls.f2 = TrFeed("ttl2", "l2.com", "A", "http://pic2.com")
+        f_list = [cls.f1, cls.f2]
+        cls.v_count = len(f_list)
+        session.add_all(f_list)
+        session.commit()
+
+        cls.fn1 = TrFeedNews(url="Url1", guid="g1", title="t1", link="l1", desc="d1", published="2013-12-12 12:12:12", feed_id=cls.f1.id)
+        cls.fn2 = TrFeedNews(url="Url2", guid="g2", title="t2", link="l2", desc="d2", published="2013-12-12 12:12:12", feed_id=cls.f1.id)
+        fn_list = [cls.fn1, cls.fn2]
+        cls.fn_count = len(fn_list)
+        session.add_all(fn_list)
+        session.commit()
+
+        cls.u1.feeds = [cls.f1]
+        session.commit()
+
+        session.close()
+
+    @classmethod
+    def tearDownClass(cls):
+
+        session = Session()
+
+        users = session.query(TrUser).all()
+        map(session.delete, users)
+
+        feeds = session.query(TrFeed).all()
+        map(session.delete, feeds)
+
+        session.commit()
+
+        session.close()
+
+
+    def test_get_feed_news_normal(self):
+
+        self.session.add(self.u1)
+
+        data = server.s_getFeedNews(self.u1.id, 0)
+
+        self.assertJsonRpc(data)
+        self.assertEquals(len(data['result']), 2)
+        self.assertEquals(data['result'][0]['title'], u't1')
+        self.assertEquals(data['result'][0]['fav'], False)
+        self.assertEquals(data['result'][1]['title'], u't2')
+
+
+    @unittest.skip("test_get_feed_news_since")
+    def test_get_feed_news_since(self):
+        pass
+
+
 class FeedAddFavTestCase(BaseTestCase):
 
     @classmethod
@@ -509,6 +580,7 @@ def suite():
     suite.addTests(loader.loadTestsFromTestCase(FeedGetTestCase))
     suite.addTests(loader.loadTestsFromTestCase(FeedSubTestCase))
     suite.addTests(loader.loadTestsFromTestCase(FeedUnsubTestCase))
+    suite.addTests(loader.loadTestsFromTestCase(FeedGetNewsTestCase))
     suite.addTests(loader.loadTestsFromTestCase(FeedAddFavTestCase))
     suite.addTests(loader.loadTestsFromTestCase(FeedDelFavTestCase))
     suite.addTests(loader.loadTestsFromTestCase(FeedGetFavsTestCase))
